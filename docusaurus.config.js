90bd778e70329f8ee9528c52f3ff4783346d8988
Arthur Cosentino <arthurcose@gmail.com>
2023-03-22 01:42:55 -0400

    Add editUrl and throw on broken links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -31,7 +31,7 @@ const config = {
   projectName: "documentation-website", // Usually your repo name.
   trailingSlash: true,
 
-  onBrokenLinks: "warn",
+  onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
 
   customFields: {
@@ -56,8 +56,8 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          // editUrl:
-          //   "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl:
+            "https://github.com/Real-PET/documentation-website/tree/master/",
         },
         blog: false,
         theme: {
