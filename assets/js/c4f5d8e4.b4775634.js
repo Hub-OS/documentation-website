46b3305712f50db7d52771099893b5e8160c84fb
Arthur Cosentino <arthurcose@gmail.com>
2023-03-23 18:23:58 -0400

    Deploy website - based on 3f483a9d47ab2266ea882f18ebf853c4fb18e9f0

---
+++ assets/js/c4f5d8e4.b4775634.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocumentation=self.webpackChunkdocumentation||[]).push([[195],{2841:(e,t,n)=>{n.r(t),n.d(t,{default:()=>s});var o=n(7294),c=n(2263),l=n(179);function s(){const e=(0,c.Z)();return(0,o.useEffect)((()=>{"development"!=e.siteConfig.customFields.NODE_ENV&&window.location.replace("https://real-pet.konstinople.dev")}),[]),o.createElement(l.Z,null)}}}]);
