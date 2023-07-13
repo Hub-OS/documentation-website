ee71eddff2a5eee6c54c909abf3cf610eac183a2
Arthur Cosentino <arthurcose@gmail.com>
2023-07-12 21:39:29 -0400

    Update redirect

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,9 @@
 import React, { useEffect } from "react";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 
 export default function Home() {
-  const context = useDocusaurusContext();
-
   useEffect(() => {
-    if (context.siteConfig.customFields.NODE_ENV != "development") {
-      window.location.replace("https://hubos.dev");
-    }
+    window.location.replace("/client/packages");
   }, []);
 
   return <Layout />;
