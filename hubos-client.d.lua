46e14ebc59ec4d8e2ddad0a365f71eb143c9b0f8
Arthur Cosentino <arthurcose@gmail.com>
2026-01-15 14:53:41 -0500

    Deploy website - based on 6ede3ed9d5eec14e80ecf08544f26e094e66a08b

--- hubos-client.d.lua
+++ hubos-client.d.lua
@@ -316,6 +316,8 @@ Input = {
 ---@field intro_func fun(self: Entity): Action|nil
 --- Used to handle movement input, setting this overrides the default handling.
 ---@field movement_func fun(self: Entity, direction: Direction)
+--- Used to generate movement input, setting this overrides the input gathering method.
+---@field movement_input_func fun(self: Entity): Direction?
 --- Will not be called if there's no matching `calculate_card_charge_time_func`
 --- 
 --- An [Action](https://docs.hubos.dev/client/lua-api/attack-api/action) or `nil` is expected as a return value.
@@ -514,6 +516,10 @@ Field = {}
 --- 
 --- Used to handle movement input.
 ---@field movement_func fun(self: Augment, direction: Direction)
+--- Overrides [player.movement_input_func](https://docs.hubos.dev/client/lua-api/entity-api/player#playermovement_input_func--functionself-direction).
+--- 
+--- Used to generate movement input.
+---@field movement_input_func fun(self: Augment): Direction?
 --- Will not be called if there's no matching `calculate_card_charge_time_func`
 --- 
 --- An [Action](https://docs.hubos.dev/client/lua-api/attack-api/action) or `nil` is expected as a return value.
@@ -553,6 +559,11 @@ Augment = {}
 --- 
 --- Used to handle movement input.
 ---@field movement_func fun(self: PlayerForm)
+--- Overrides [player.movement_input_func](https://docs.hubos.dev/client/lua-api/entity-api/player#playermovement_input_func--functionself-direction) when this form is active.
+--- Also overrides any [Augment](https://docs.hubos.dev/client/lua-api/entity-api/player#augment)'s override.
+--- 
+--- Used to generate movement input.
+---@field movement_input_func fun(self: PlayerForm): Direction?
 --- Will not be called if there's no matching `calculate_card_charge_time_func`
 --- 
 --- An [Action](https://docs.hubos.dev/client/lua-api/attack-api/action) or `nil` is expected as a return value.
@@ -4162,6 +4173,22 @@ function AuxProp:require_card_tag(tag) end
 ---@param tag string
 ---@return AuxProp
 function AuxProp:require_card_not_tag(tag) end
+
+--- - Body priority
+--- - `hit_flags`: [Hit](https://docs.hubos.dev/client/lua-api/attack-api/hit-props#hit_propsflags)
+--- 
+--- The AuxProp will check active status [flags](https://docs.hubos.dev/client/lua-api/attack-api/hit-props#hit_propsflags) to see if all matching flags apply.
+---@param hit_flags Hit | number
+---@return AuxProp
+function AuxProp:require_statuses(hit_flags) end
+
+--- - Body priority
+--- - `hit_flags`: [Hit](https://docs.hubos.dev/client/lua-api/attack-api/hit-props#hit_propsflags)
+--- 
+--- The AuxProp will check active status [flags](https://docs.hubos.dev/client/lua-api/attack-api/hit-props#hit_propsflags) to verify no flags match.
+---@param hit_flags Hit | number
+---@return AuxProp
+function AuxProp:require_statuses_absent(hit_flags) end
 
 --- - HP Expression priority
 --- - `expr`: [Math Expression String](https://docs.hubos.dev/client/lua-api/defense-api/aux-prop#math-expression-strings), `"DAMAGE"` will represent the total damage value for all incoming hits.
